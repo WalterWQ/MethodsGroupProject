07faa1621815efa41fb02f4f74aeaf29fa7de813
Carter Morrison <40690697@live.napier.ac.uk>
2024-10-08 20:16:53 +0100

    Added More Characters To Test Message
    Carter Morrison <40690697@live.napier.ac.uk>
    
    Signed-off-by: Carter Morrison <40690697@live.napier.ac.uk>

--- src/main/java/group8/project_files/Main.java
+++ src/main/java/group8/project_files/Main.java
@@ -2,6 +2,6 @@ package group8.project_files;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worlds!");
     }
 }
