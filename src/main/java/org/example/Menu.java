1ea5b0d8bce51e9a6b115a336a8ce103cb3fa813
Adrian Cis <40646652@live.napier.ac.uk>
2024-10-15 17:34:52 +0100

    Started work on the menu.
    Signed-off-by: M6xxy <79139144+M6xxy@users.noreply.github.com>

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.*;
+/**
+ * This is the class for the application menu, it will take in a input from user and run a process dependant on it.
+ */
+public class Menu {
+    //Declaring Varibles
+    private int userChoice = -1;
+
+    /**
+     * This class runs the menu and takes in a user choice and acts upon it
+     */
+    public void menu() {
+        while (userChoice == -1) {}
+    }
+}
