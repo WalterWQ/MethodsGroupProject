1387b95a99368a2d2c5406c513deaf0589257b47
Adrian Cis <40646652@live.napier.ac.uk>
2024-10-25 01:31:37 +0100

    Created Feature-01 method and commented
    Signed-off-by: M6xxy <79139144+M6xxy@users.noreply.github.com>

--- src/main/java/group8/project_files/Database.java
+++ src/main/java/group8/project_files/Database.java
@@ -12,7 +12,6 @@ public class Database {
     static String url = "jdbc:sqlite:db.sqlite"; // Database file
 
     public void testConnect() {
-
         // DEBUG DATABASE CONNECTION
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) { // if connects
@@ -25,5 +24,13 @@ public class Database {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * This filers the amount of top populated cities by amount selected by user in area selected by user
+     * @param type  0 = world, 1 = continent, 2 = region, 3 = country, 4 = district
+     */
+    public void getTopCities(int topAmount, int type) {
+
     }
 }
